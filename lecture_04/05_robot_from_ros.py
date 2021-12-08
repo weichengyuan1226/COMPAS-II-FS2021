ab6d3cd34080aad6c7e138c6d031aa049c846ebd
weichengyuan1226 <morphing.program@gmail.com>
2021-12-08 12:29:13 -0500

    changes

--- lecture_04/05_robot_from_ros.py
+++ lecture_04/05_robot_from_ros.py
@@ -4,6 +4,6 @@ from compas_fab.backends import RosClient
 
 # Load robot without geometry
 with RosClient('localhost') as ros:
-    robot = ros.load_robot(load_geometry=False)
+    robot = ros.load_robot(load_geometry = False)
     # robot already contains model, semantics and client
     robot.info()
